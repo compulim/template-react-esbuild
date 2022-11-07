523671a94cd2730a4aea0f4e5dfc42ed5a76b929
William Wong <compulim@hotmail.com>
2022-11-07 14:47:20 -0800

    Clean up

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,4 @@ import App from './ui/App';
 
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error('Document must contains <div id="root">.');
-}
-
-createRoot(rootElement).render(<App />);
+rootElement && createRoot(rootElement).render(<App />);
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <h1>
-      Hello, World from <code>template-react-esbuild</code>!
+      Hello, World!
     </h1>
   );
 };
