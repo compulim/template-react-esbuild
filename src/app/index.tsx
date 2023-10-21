3e67a7fee918e8823f11a401ff4a0b80ce6e4c5e
William Wong <compulim@users.noreply.github.com>
2023-10-21 13:08:09 -0700

    Remove ID for <main>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 
 import App from './ui/App';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementsByTagName('main')[0];
 
 rootElement &&
   createRoot(rootElement).render(
